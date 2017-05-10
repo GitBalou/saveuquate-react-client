8582e5850e5d5954f97d0c995ee1ff7681d8b82d
gabriel <pour.blaireaux@yahoo.fr>
2017-05-10 13:24:05 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => (
+    <h1>Coucou</h1>
+);
+
+// Rendering
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
